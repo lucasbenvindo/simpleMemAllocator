113aad423ed729bfdd576964535c1b56b9261e2a
lucasbenvindo <lucasmbenvindo@icloud.com>
2026-02-03 15:01:43 +0000

    code

---
+++ allocator.c
@@ -0,0 +1,48 @@
+#define HEAP_SIZE 4096
+static unsigned char heap[HEAP_SIZE]; 
+
+typedef struct chunk {
+		size_t size;
+		int free;
+		struct chunk *next;
+} chunk_t;
+
+static chunk_t *heap_start = NULL;
+
+void *my_malloc(size_t size) {
+		if(size == 0)
+			return NULL;
+			
+		if(heap_start == NULL) {
+				chunk_t *chunk = (chunk_t *)heap;
+				
+				chunk->size = size;
+				chunk->free = 0;
+				chunk->next = NULL;
+				
+				heap_start = chunk;
+				
+				return (void *)(chunk + 1);
+			}
+			
+			chunk_t *curr = heap_start;
+			
+			while(curr) {
+					if(curr->free && curr->size >=size) {
+							curr->free = 0;
+							return (void *)(curr + 1);
+						}
+						curr = curr->next;
+				}
+				
+				return NULL;
+			
+}
+
+void my_free(void *ptr) {
+		if(!ptr)
+			return;
+			
+		chunk_t *chunk = (chunk_t *)ptr - 1;
+		chunk->free = 1;
+}
